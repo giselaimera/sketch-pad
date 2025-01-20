f1fc5a9edf7e03c4da41ac5cf1114c623e9a41bc
giselaimera <imeragisela@gmail.com>
2025-01-20 17:57:07 +0100

    Grid is created and the number of squares inside the container can only be hard-coded

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-const GRID_SIZE=16;
-const container=document.querySelector("#container");
-for (let i=0;i<GRID_SIZE;)
+const GRID_SIZE=10;
+const squareSize=(100/GRID_SIZE);
+const container=document.querySelector(".container");
+for (let i=0;i<Math.pow(GRID_SIZE,2);i++){
+    const square=document.createElement("div");
+    square.classList.add("square");
+    square.style.border = "1px solid #000";
+    square.style.flexBasis=`${squareSize}%`;
+    container.appendChild(square);
+}
